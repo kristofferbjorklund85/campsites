1afbfae548c44a2885c46ef1c40e9db8224ab6a4
kristofferbjorklund85 <kristofferbjorklund85@gmail.com>
2017-11-09 12:59:12 +0100

    Testcommit

--- app/src/main/java/com/example/sombra/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sombra/myapplication/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // Start the app
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
