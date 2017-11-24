d7cfaebbef716e5f63a217c8236a13d0dc39206e
Pregmark <sam.pregmark@gmail.com>
2017-11-24 11:54:43 +0100

    Added some Marker functionality in Maps

--- app/src/main/java/com/example/sombra/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/sombra/myapplication/MapsActivity.java
@@ -1,18 +1,31 @@
 package com.example.sombra.myapplication;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+    private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+
+    private Marker mPerth;
+    private Marker mSydney;
+    private Marker mBrisbane;
+
+    private static GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +37,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
+    private static void createMarkers(List<CampsiteModel> list) {
+        for(CampsiteModel cm : list) {
+            mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(cm.lat, cm.long))
+                    .title()
+        }
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -37,10 +56,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        createMarkers(list);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(BRISBANE));
+
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Intent intent = new Intent(MapsActivity.this, Campsite.class);
+                intent.putExtras((CampsiteModel) marker.getTag(campsiteModel));
+                startActivity(intent);
+            }
+        });
     }
 }
