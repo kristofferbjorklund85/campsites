f32f325fbbedfa00a33ea58297800d006e1754a1
Pregmark <sam.pregmark@gmail.com>
2017-11-27 15:15:19 +0100

    Refractored CampsiteForm into PostActivity

--- app/src/main/java/com/example/sombra/myapplication/PostActivity.java
+++ app/src/main/java/com/example/sombra/myapplication/PostActivity.java
@@ -2,6 +2,9 @@ package com.example.sombra.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.model.LatLng;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras == null) {
+            return;
+        }
+
+        LatLng latLng = extras.getParcelable("latlng");
+
+        double lat = latLng.latitude;
+        double lng = latLng.longitude;
+
+        /*TextView textView = (TextView) findViewById(R.id.latlng);
+        textView.setText("Latitude: " + Double.toString(lat) + "\nLongitude: " + Double.toString(lng));*/
     }
 }
