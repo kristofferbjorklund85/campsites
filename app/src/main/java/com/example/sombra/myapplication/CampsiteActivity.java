6cb7da2a82d79565982890d9bf774fc5cec12faf
kristofferbjorklund85 <kristofferbjorklund85@gmail.com>
2017-11-28 15:57:31 +0100

    Added comments to CampsiteActivity

--- app/src/main/java/com/example/sombra/myapplication/CampsiteActivity.java
+++ app/src/main/java/com/example/sombra/myapplication/CampsiteActivity.java
@@ -1,13 +1,25 @@
 package com.example.sombra.myapplication;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class CampsiteActivity extends AppCompatActivity {
 
@@ -50,8 +62,75 @@ public class CampsiteActivity extends AppCompatActivity {
         capacityView.setText("Capacity: " + cm.capacity);
         availabilityView.setText("Availability: " + cm.availability);
         descriptionView.setText("Description: " + cm.description);
+
+        loadComments();
     }
 
+    public void loadComments() {
+
+        Comment[] commentsArray = {
+                new Comment(User.getUsername(), "This is great campsite, many friendly people"),
+                new Comment(User.getUsername(), "Too many germans, 0/10"),
+                new Comment(User.getUsername(), "BUY VIAGRA DELUXE NICE PRICE")};
+
+
+        ListView comments = (ListView) findViewById(R.id.comments_listview);
+        CommentAdapter adapter = new CommentAdapter(this,
+                R.layout.comments_listitem, commentsArray);
+        comments.setAdapter(adapter);
+
+    }
+
+    private class CommentAdapter extends ArrayAdapter<Comment> {
+
+        Context context;
+        int layoutResourceId;
+        Comment data[] = null;
+
+
+        public CommentAdapter(Context context, int layoutResourceId, Comment[] data) {
+            super(context, layoutResourceId, data);
+            this.layoutResourceId = layoutResourceId;
+            this.context = context;
+            this.data = data;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row = convertView;
+            CommentHolder holder = null;
+
+            if(row == null) {
+                LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+                row = inflater.inflate(layoutResourceId, parent, false);
+
+                holder = new CommentHolder();
+
+                holder.user = (TextView)row.findViewById(R.id.comment_user);
+                holder.comment = (TextView)row.findViewById(R.id.comment_textview);
+
+
+                row.setTag(holder);
+
+            } else {
+                holder = (CommentHolder)row.getTag();
+            }
+
+            holder.user.setText(data[position].username + ":");
+            holder.comment.setText(data[position].comment);
+
+
+            return row;
+        }
+        
+
+    }
+
+    static class CommentHolder
+    {
+        TextView user;
+        TextView comment;
+    }
 
 
 
---
+++ app/src/main/java/com/example/sombra/myapplication/Comment.java
@@ -0,0 +1,17 @@
+package com.example.sombra.myapplication;
+
+/**
+ * Created by Sombra on 2017-11-28.
+ */
+
+public class Comment {
+
+    public String username;
+    public String comment;
+
+    public Comment(String user, String comment) {
+        this.username = user;
+        this.comment = comment;
+    }
+
+}
