ff5899f7edb7a67c80cf0be4149de7d67c322851
kristofferbjorklund85 <kristofferbjorklund85@gmail.com>
2017-11-28 12:11:14 +0100

    Added landing page, resource for hamburger

---
+++ app/src/main/java/com/example/sombra/myapplication/LandingActivity.java
@@ -0,0 +1,59 @@
+package com.example.sombra.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class LandingActivity extends AppCompatActivity {
+
+    VolleyHandler vh;
+    ArrayList<CampsiteModel> cml;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        vh = new VolleyHandler();
+        vh.getCampsites(this);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    public void loadMaps(View view) {
+        cml = (ArrayList<CampsiteModel>) vh.getCampList();
+
+        for(CampsiteModel cm : cml) {
+            Log.d("List before: ", cm.location);
+        }
+
+        Log.d("starting: ", "Maps");
+        Intent intent = new Intent(LandingActivity.this, MapsActivity.class);
+        intent.putParcelableArrayListExtra("cmList", cml);
+        startActivity(intent);
+        finish();
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/sombra/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/sombra/myapplication/LoginActivity.java
@@ -11,33 +11,22 @@ import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
 
-    VolleyHandler vh;
-    ArrayList<CampsiteModel> cml;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        vh = new VolleyHandler();
-        vh.getCampsites(this);
-
     }
 
     public void login(View view) {
-        cml = (ArrayList<CampsiteModel>) vh.getCampList();
-
-        for(CampsiteModel cm : cml) {
-            Log.d("List before: ", cm.location);
-        }
-
         EditText et = findViewById(R.id.input_username);
         User.setUsername(et.getText().toString());
         Log.d("Username: ", User.getUsername());
 
-        Log.d("starting: ", "Maps");
-        Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
-        intent.putParcelableArrayListExtra("cmList", cml);
+        Log.d("starting: ", "Landing");
+        Intent intent = new Intent(LoginActivity.this, LandingActivity.class);
         startActivity(intent);
         finish();
     }
