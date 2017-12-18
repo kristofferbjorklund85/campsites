3cda7a8097276f5297854b6f446cd873ce6bf080
Pregmark <sam.pregmark@gmail.com>
2017-12-18 14:03:16 +0100

    Added wrong input handling in login and created a class to check for wrong input

---
+++ app/src/main/java/com/example/sombra/myapplication/FieldChecker.java
@@ -0,0 +1,42 @@
+package com.example.sombra.myapplication;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Samuel on 2017-12-18.
+ */
+
+public class FieldChecker {
+    public static boolean checkString(Context c, String s, String cs) {
+        Pattern pattern = Pattern.compile("[~#@*+%{}<>\\[\\]|\"\\_^]");
+        Matcher matcher = pattern.matcher(s);
+        if(s.isEmpty() || s == null) {
+            toast(c, cs + " cannot be empty", "short");
+            return false;
+        }
+        else if(matcher.find()) {
+            toast(c, cs + " cannot contain [~#@*+%{}<>[]|\"_^!?]", "short");
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
+
+    public static void toast(Context c, String s, String length) {
+        if(length == "short") {
+            Toast toast = Toast.makeText(c, s, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        else {
+            Toast toast = Toast.makeText(c, s, Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+}
--- app/src/main/java/com/example/sombra/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/sombra/myapplication/LoginActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
-
 
 
     @Override
@@ -18,25 +18,26 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        if(true) {
-            User.setUsername("Satan666");
+        /*if (false) {
+            User.setUsername("JanBanan");
             User.setAppContext(this.getApplicationContext());
             Log.d("starting: ", "Landing");
             Intent intent = new Intent(LoginActivity.this, LandingActivity.class);
             startActivity(intent);
             finish();
-        }
+        }*/
     }
 
     public void login(View view) {
-        EditText et = findViewById(R.id.input_username);
-        User.setUsername(et.getText().toString());
-        Log.d("Username: ", User.getUsername());
-
-        Log.d("starting: ", "Landing");
-        Intent intent = new Intent(LoginActivity.this, LandingActivity.class);
-        startActivity(intent);
-        finish();
+        EditText un = findViewById(R.id.input_username);
+        EditText pw = findViewById(R.id.input_password);
+        if (FieldChecker.checkString(this, un.getText().toString(), "Username") &&
+                FieldChecker.checkString(this, pw.getText().toString(), "Password")) {
+            User.setUsername(un.getText().toString());
+            User.setAppContext(this.getApplicationContext());
+            Intent intent = new Intent(LoginActivity.this, LandingActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
-
 }
