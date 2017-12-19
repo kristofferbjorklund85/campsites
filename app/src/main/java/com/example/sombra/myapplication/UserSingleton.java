7d4418390ff50f41d94c28ffc2921d9f8c078887
Pregmark <sam.pregmark@gmail.com>
2017-12-19 18:36:53 +0100

    Refactored User to UserSingleton

---
+++ app/src/main/java/com/example/sombra/myapplication/UserSingleton.java
@@ -0,0 +1,25 @@
+package com.example.sombra.myapplication;
+
+import android.content.Context;
+
+/**
+ * Created by Sombra on 2017-11-27.
+ */
+
+public class UserSingleton {
+    private static String username = null;
+    private static String id = null;
+    private static Context context;
+
+    public static void setUsername(String user) {UserSingleton.username = user;}
+
+    public static void setId(String id) {UserSingleton.id = id;}
+
+    public static void setAppContext(Context context) {UserSingleton.context = context;}
+
+    public static String getUsername() {return username;}
+
+    public static String getId() {return id;}
+
+    public static Context getAppContext() {return context;}
+}
